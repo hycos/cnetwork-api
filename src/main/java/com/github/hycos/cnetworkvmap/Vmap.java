fe7aa1f4279c6ed09fec4988521a43b3d3b0723b
Julian Thome <julian.thome@uni.lu>
2017-10-16 11:56:17 +0200

    cnetwork api

---
+++ src/main/java/com/github/hycos/cnetworkvmap/Vmap.java
@@ -0,0 +1,18 @@
+package com.github.hycos.cnetworkvmap;
+
+import com.github.hycos.cnetwork.core.domain.NodeDomain;
+import com.github.hycos.cnetwork.core.graph.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vmap {
+
+    private Map<Node, NodeDomain> vmap = new HashMap<>();
+
+
+
+
+
+
+}
---
+++ src/test/java/TestVmap.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class TestVmap {
+    @Test
+    public void testVmap() {
+
+    }
+}
