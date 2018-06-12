501e42f5de5aed89c4f5d52d11a2d1d09d1a7924
Julian Thome <julian.thome@uni.lu>
2018-06-12 17:28:57 +0200

    readme.md

--- src/main/java/com/github/hycos/cnetwork/sig/JavaType.java
+++ src/main/java/com/github/hycos/cnetwork/sig/JavaType.java
@@ -1,20 +1,3 @@
-/*
- * cnetwork - a constraint network implementation for Java
- * Copyright (C) 2017 Julian Thome <julian.thome.de@gmail.com>
- *
- * cnetwork is licensed under the EUPL, Version 1.1 or – as soon
- * they will be approved by the European Commission - subsequent versions of the
- * EUPL (the "Licence"); You may not use this work except in compliance with the
- * Licence. You may obtain a copy of the Licence at:
- *
- * https://joinup.ec.europa.eu/sites/default/files/eupl1.1.-licence-en_0.pdf
- *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.  See the Licence for the
- * specific language governing permissions and limitations under the Licence.
- */
-
 /**
  * This file is part of the Joana IFC project. It is developed at the
  * Programming Paradigms Group of the Karlsruhe Institute of Technology.
@@ -22,6 +5,12 @@
  * For further details on licensing please read the information at
  * http://joana.ipd.kit.edu or contact the authors.
  */
+
+// @TODO: This code is stemming from the JOANA project
+// https://github.com/joana-team/joana
+// @TODO: refactor
+
+
 package com.github.hycos.cnetwork.sig;
 
 import java.util.LinkedList;
